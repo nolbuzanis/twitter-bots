cfc42906c43c6d90fde4e058b82128350dbcf980
nolbuzanis <nolanbuzanis@gmail.com>
2022-02-05 15:08:29 -0500

    feat: mongo insert functions

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -2,13 +2,12 @@ import { MongoClient } from 'mongodb';
 
 const uri = process.env.MONGODB_URI;
 const options = {};
+const dbName = process.env.DATABASE_NAME || 'development';
 
-let client;
-let clientPromise;
+type CollectionName = 'highlights';
 
-if (!uri) {
-  throw new Error('Please add your Mongo URI to .env.local');
-}
+const client = new MongoClient(uri || '', options);
+const clientPromise = client.connect();
 
 // if (process.env.NODE_ENV === 'development') {
 //   // In development mode, use a global variable so that the value
@@ -20,9 +19,32 @@ if (!uri) {
 //   clientPromise = global._mongoClientPromise;
 // } else {
 // In production mode, it's best to not use a global variable.
-client = new MongoClient(uri, options);
-clientPromise = client.connect();
+
 // }
+
+export async function insertOne(collectionName: CollectionName, doc: {}) {
+  try {
+    const collection = (await clientPromise)
+      .db(dbName)
+      .collection(collectionName);
+
+    return collection.insertOne(doc);
+  } catch (error) {
+    return { error };
+  }
+}
+
+export async function insertMany(collectionName: CollectionName, docs: {}[]) {
+  try {
+    const collection = (await clientPromise)
+      .db(dbName)
+      .collection(collectionName);
+
+    return collection.insertMany(docs);
+  } catch (error) {
+    return { error };
+  }
+}
 
 // Export a module-scoped MongoClient promise. By doing this in a
 // separate module, the client can be shared across functions.
